bf7a503daea3e3a2908c47a80c23024b1d4c312e
SonnyQin <Sonny.Qin25@nflsbca.com>
2024-03-04 18:51:52 +0800

    Add Loss Function TO KNN

--- KNN_Prediction.py
+++ KNN_Prediction.py
@@ -5,16 +5,21 @@ import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.model_selection import train_test_split
+
+a=np.array([1,2,3,4,5,6])
+b=np.array([6,5,4,3,2,1])
+
+def arrange(data):
+    data=np.array(data)
+    x=np.delete(data, 0, 1)
+    x=x.astype(np.float64)
+    return x
+
 #依次训练各个特征, data是np.mat
-def kNN_Prediction(data, k=7, predictNum=100):
+def kNN_Prediction(data, k=5, predictNum=50):
     data=np.array(data)
     #后续50个数据的存储, 记录未归一化的数据
     predict=[data[-1][1:],]
-    print(data[-1])
-    # #遍历五个特征, 并获取到5个特征的分类器
-    # classifiers=[]
-    # #用于记录各个特征的标签
-    # targets=[]
     
     #整理特征数据
     #去除日期
@@ -36,7 +41,7 @@ def kNN_Prediction(data, k=7, predictNum=100):
     kNN.fit(x,y)
     
     #推测数据
-    for i in range(1, predictNum+1):
+    for i in range(1, predictNum):
         distance, indices=kNN.kneighbors(scaler.transform([predict[i-1]]))
         distance=distance+0.00000001
         distance=1/distance
@@ -55,13 +60,40 @@ def kNN_Prediction(data, k=7, predictNum=100):
         results=results.reshape(-1)
         #results=[[d[0] for d in results]]
         predict.append(results)
-        print(results)
-    return np.array(predict)
+        #print(results)
+    return np.array(predict).astype(np.float64)
+
+def loss(x, y):
+    return np.sum((x-y)*(x-y))
+    
+
+#全局最佳
+def paraAdjustW(data, t, kMax=100):
+    minn=np.inf
+    k=0
+    t=arrange(t)    #t现在是np.array
+    outResults=[]
+    for i in range(1, kMax):
+        results=kNN_Prediction(data, i)
+        l=loss(results, t)
+        if  l< minn:
+            minn=l
+            k=i
+            outResults=results
+        print(k)
+        print(minn)
+    
+    return k, minn
+        
      
 if __name__=='__main__':
+    name='MCD'
+    
     data=Preprocess.load_data()
-    results=kNN_Prediction(data['ALL'][:-50])
-    x=results[:,3]
-    x=x.astype(np.float64)
-    plt.plot(x,linewidth=1, color='red')
+
+    x=data[name][:-51].copy()
+    y=data[name][-50:].copy()
+    k,min=paraAdjustW(x,y)
+    results=kNN_Prediction(data[name][:-51],k,50)
+    plt.plot(results[:,3],linewidth=1, color='red')
     plt.show()
--- test.py
+++ test.py
@@ -1,10 +1,1 @@
-from sklearn.preprocessing import MinMaxScaler
-import numpy as np
-# 创建数据
-data = np.array([[1, 2], [3, 4], [5, 6]])
-# 创建MinMaxScaler对象
-scaler = MinMaxScaler()
-# 对数据进行缩放处理
-scaled_data = scaler.fit_transform(data)
-mydata=np.array([[0.1,0.2],[0.3,0.4],[0.5,0.6]])
-print(scaler.inverse_transform(mydata))
+from m
